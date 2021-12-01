3c0dbb8507e2113b81c3c9e0e8812f73be0c7d14
Jeremy Madden <jeremy.madden@zapproved.com>
2021-12-01 15:47:18 -0800

    Second puzzle!

--- Day01_SonarSweep/Program.cs
+++ Day01_SonarSweep/Program.cs
@@ -6,17 +6,40 @@ using System.Reflection;
 
 namespace Day01_SonarSweep
 {
+    internal class SlidingWindow
+    {
+        public int FirstWindow { get; set; }
+        public int SecondWindow { get; set; }
+        public int ThirdWindow { get; set; }
+        public int Total => FirstWindow + SecondWindow + ThirdWindow;
+
+        public override string ToString()
+        {
+            return $"{FirstWindow} + {SecondWindow} + {ThirdWindow} = {Total}";
+        }
+    }
     internal class Program
     {
         public static void Main(string[] args)
         {
             const string inputFileName = "input.txt";
             var inputValues = ReadInput(inputFileName).Select(int.Parse).ToList();
-            var diffs = inputValues.Skip(1).Zip(inputValues, GetDifferential).ToList();
+            var sliders = inputValues
+                .Zip(inputValues.Skip(1), GetSlidingWindow)
+                .Zip(inputValues.Skip(2), AddToSlidingWindow)
+                .ToList();
+            // sliders.ForEach(Console.WriteLine);
+            var sliderTotals = sliders.Select(s => s.Total).ToList();
+            var diffs = GetDifferentials(sliderTotals);
             // diffs.ForEach(Console.WriteLine);
             
             var increases = diffs.Count(d => d.Delta > 0);
             Console.WriteLine($"{increases} measurements larger than the previous measurement.");
+        }
+
+        private static List<Differential> GetDifferentials(List<int> inputValues)
+        {
+            return inputValues.Skip(1).Zip(inputValues, GetDifferential).ToList();
         }
 
         private static IEnumerable<string> ReadInput(string fileName)
@@ -43,5 +66,18 @@ namespace Day01_SonarSweep
                 CurrentValue = current,
                 PreviousValue = previous
             };
+
+        private static SlidingWindow GetSlidingWindow(int first, int second) =>
+            new SlidingWindow
+            {
+                FirstWindow = first,
+                SecondWindow = second
+            };
+
+        private static SlidingWindow AddToSlidingWindow(SlidingWindow slidingWindow, int third)
+        {
+            slidingWindow.ThirdWindow = third;
+            return slidingWindow;
+        }
     }
 }
